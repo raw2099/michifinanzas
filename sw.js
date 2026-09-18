cc2519874f20d94cc5e772b86161174bb17e2c5a
raw2099 <eddyro2001@gmail.com>
2026-09-18 17:44:08 -0500

    Add service worker

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE='michifinanzas-v68';
+const CORE=['./','./index.html','./manifest.webmanifest','./icons/icon.svg','./config.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  const u=new URL(e.request.url);
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r}).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  if(u.origin===location.origin){
+    e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+  }
+});
