79f54c4505a8ef7b520b879cfdc93fd2aab227a1
raw2099 <eddyro2001@gmail.com>
2026-09-19 11:16:04 -0500

    chore: bump PWA cache for V7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='michifinanzas-v69';
+const CACHE='michifinanzas-v70';
 const CORE=['./','./index.html','./manifest.webmanifest','./icons/icon.svg','./config.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
